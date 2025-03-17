e5cf101acaeca5d31a8d79f691ba7608765b3647
Allegra <allegraruthedimo@gmail.com>
2025-03-17 04:21:10 +0100

    ajout de certains fichiers après implémentation d'un service de création de billets de blog

--- fotoblog/urls.py
+++ fotoblog/urls.py
@@ -9,25 +9,27 @@ import blog.views
 import authentication.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', LoginView.as_view(
-        template_name='authentication/login.html',
+    path("admin/", admin.site.urls),
+    path("", LoginView.as_view(
+        template_name="authentication/login.html",
         redirect_authenticated_user=True),
-         name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('change-password/', PasswordChangeView.as_view(
-        template_name='authentication/password_change_form.html'),
-         name='password_change'
+         name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("change-password/", PasswordChangeView.as_view(
+        template_name="authentication/password_change_form.html"),
+         name="password_change"
          ),
-    path('change-password-done/', PasswordChangeDoneView.as_view(
-        template_name='authentication/password_change_done.html'),
-         name='password_change_done'
+    path("change-password-done/", PasswordChangeDoneView.as_view(
+        template_name="authentication/password_change_done.html"),
+         name="password_change_done"
          ),
-    path('signup/', authentication.views.signup_page, name='signup'),
-    path('profile_photo/upload/', authentication.views.upload_profile_photo, 
-         name='upload_profile_photo'),
-    path('home/', blog.views.home, name='home'),
-    path('photo/upload/', blog.views.photo_upload, name='photo_upload'),
+    path("signup/", authentication.views.signup_page, name="signup"),
+    path("profile_photo/upload/", authentication.views.upload_profile_photo, 
+         name="upload_profile_photo"),
+    path("home/", blog.views.home, name="home"),
+    path("photo/upload/", blog.views.photo_upload, name="photo_upload"),
+    path("blog/create/", blog.views.blog_and_photo_upload, name="blog_create"),
+    path("blog/<int:blog_id>", blog.views.view_blog, name="view_blog"),
 ]
 if settings.DEBUG:
     urlpatterns += static(
