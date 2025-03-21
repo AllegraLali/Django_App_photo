eca5ad88c641c886a0644126d1b82bc19dc9d01f
Allegra <allegraruthedimo@gmail.com>
2025-03-21 02:20:38 +0100

    ajout de certains fichiers après création d'une relation plusieurs à plusieurs

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import get_user_model
 from django import forms
 from . import models
+
+User = get_user_model()
 
 class PhotoForm(forms.ModelForm):
     class Meta:
@@ -17,3 +20,8 @@ class BlogForm(forms.ModelForm):
 class DeleteBlogForm(forms.Form):
     delete_blog = forms.BooleanField(widget=forms.HiddenInput, 
                                      initial=True)
+    
+class FollowUsersForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ["follows"]
